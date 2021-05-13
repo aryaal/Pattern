5b5164a3f0769bf425031f345385754b215d59b6
aryaal <84003401+aryaal@users.noreply.github.com>
2021-05-13 12:32:15 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,25 @@
+
+public class Pattern
+{
+	public static void main(String[] args)
+	{	int n=3;
+		int star=1;
+		
+		for(int i=n;i>=1;i--)
+		{
+			for(int space=i-1;space>=1;space--)
+			{
+				System.out.print(" ");
+			}
+			
+		
+		for(int j=1;j<=star;j++)
+		{
+			
+			System.out.print("*");
+		}
+		System.out.println();
+		star+=2;
+		}
+	}
+}
